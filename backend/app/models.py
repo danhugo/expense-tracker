33ed77f624c5d110bb71c9a7d0c48c70900c174d
danhugo <hungdd2604@gmail.com>
2025-05-07 08:24:59 +0900

    update models

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, String, Boolean
 from .database import Base
-# # from sqlalchemy import Column, Integer, String, Float, Date, Boolean
+# from sqlalchemy import Column, Integer, String, Float, Date, Boolean
 
 class User(Base):
     __tablename__ = "users"
@@ -10,26 +10,3 @@ class User(Base):
     hashed_password = Column(String)
     is_verified = Column(Boolean, default=False)
 
-# # class Expense(Base):
-# #     __tablename__ = "expenses"
-# 
-# #     id = Column(Integer, primary_key=True, index=True)
-# #     amount = Column(Float, nullable=False)
-# #     date = Column(Date, nullable=False)
-# #     category = Column(String, nullable=False)
-# #     description = Column(String)
-# 
-# # class Income(Base):
-# #     __tablename__ = "income"
-# 
-# #     id = Column(Integer, primary_key=True, index=True)
-# #     amount = Column(Float, nullable=False)
-# #     date = Column(Date, nullable=False)
-# #     source = Column(String, nullable=False)
-# 
-# # class Budget(Base):
-# #     __tablename__ = "budgets"
-# 
-# #     id = Column(Integer, primary_key=True, index=True)
-# #     category = Column(String, nullable=False)
-# #     amount = Column(Float, nullable=False)
